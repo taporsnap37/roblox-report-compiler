744b79ab14e4a4cb2ef49f8196cbd95c7d5508fc
Mooshi <rchiriac16@gmail.com>
2023-04-27 01:37:42 +0100

    Re coded the whole app.

---
+++ ReportCompilerApp/MainApp.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainApp
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        btnFetchID = New Button()
+        btnCopy = New Button()
+        btnClear = New Button()
+        txtUsername = New TextBox()
+        txtExploit = New TextBox()
+        GroupBox1 = New GroupBox()
+        lbl_Clip = New Label()
+        lbl_Date = New Label()
+        lbl_Exploit = New Label()
+        lbl_ID = New Label()
+        Label1 = New Label()
+        Label2 = New Label()
+        CheckBoxDate = New CheckBox()
+        CheckBoxIDOnly = New CheckBox()
+        Label3 = New Label()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' btnFetchID
+        ' 
+        btnFetchID.Location = New Point(108, 689)
+        btnFetchID.Name = "btnFetchID"
+        btnFetchID.Size = New Size(161, 64)
+        btnFetchID.TabIndex = 0
+        btnFetchID.Text = "Fetch ID"
+        btnFetchID.UseVisualStyleBackColor = True
+        ' 
+        ' btnCopy
+        ' 
+        btnCopy.Location = New Point(314, 689)
+        btnCopy.Name = "btnCopy"
+        btnCopy.Size = New Size(161, 64)
+        btnCopy.TabIndex = 1
+        btnCopy.Text = "Copy"
+        btnCopy.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(518, 689)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(161, 64)
+        btnClear.TabIndex = 2
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(359, 134)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(270, 39)
+        txtUsername.TabIndex = 3
+        ' 
+        ' txtExploit
+        ' 
+        txtExploit.Location = New Point(359, 198)
+        txtExploit.Name = "txtExploit"
+        txtExploit.Size = New Size(270, 39)
+        txtExploit.TabIndex = 4
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(lbl_Clip)
+        GroupBox1.Controls.Add(lbl_Date)
+        GroupBox1.Controls.Add(lbl_Exploit)
+        GroupBox1.Controls.Add(lbl_ID)
+        GroupBox1.Location = New Point(130, 386)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(480, 253)
+        GroupBox1.TabIndex = 5
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Formatted Report"
+        ' 
+        ' lbl_Clip
+        ' 
+        lbl_Clip.AutoSize = True
+        lbl_Clip.Location = New Point(21, 200)
+        lbl_Clip.Name = "lbl_Clip"
+        lbl_Clip.Size = New Size(231, 32)
+        lbl_Clip.TabIndex = 3
+        lbl_Clip.Text = "Clip: Attached Video"
+        ' 
+        ' lbl_Date
+        ' 
+        lbl_Date.AutoSize = True
+        lbl_Date.Location = New Point(21, 158)
+        lbl_Date.Name = "lbl_Date"
+        lbl_Date.Size = New Size(69, 32)
+        lbl_Date.TabIndex = 2
+        lbl_Date.Text = "Date:"
+        ' 
+        ' lbl_Exploit
+        ' 
+        lbl_Exploit.AutoSize = True
+        lbl_Exploit.Location = New Point(21, 116)
+        lbl_Exploit.Name = "lbl_Exploit"
+        lbl_Exploit.Size = New Size(90, 32)
+        lbl_Exploit.TabIndex = 1
+        lbl_Exploit.Text = "Exploit:"
+        ' 
+        ' lbl_ID
+        ' 
+        lbl_ID.AutoSize = True
+        lbl_ID.Location = New Point(21, 74)
+        lbl_ID.Name = "lbl_ID"
+        lbl_ID.Size = New Size(42, 32)
+        lbl_ID.TabIndex = 0
+        lbl_ID.Text = "ID:"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(130, 137)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(188, 32)
+        Label1.TabIndex = 6
+        Label1.Text = "Enter Username:"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(130, 205)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(152, 32)
+        Label2.TabIndex = 7
+        Label2.Text = "Enter Exploit:"
+        ' 
+        ' CheckBoxDate
+        ' 
+        CheckBoxDate.AutoSize = True
+        CheckBoxDate.Location = New Point(137, 278)
+        CheckBoxDate.Name = "CheckBoxDate"
+        CheckBoxDate.Size = New Size(181, 36)
+        CheckBoxDate.TabIndex = 8
+        CheckBoxDate.Text = "Include Date"
+        CheckBoxDate.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBoxIDOnly
+        ' 
+        CheckBoxIDOnly.AutoSize = True
+        CheckBoxIDOnly.Location = New Point(137, 320)
+        CheckBoxIDOnly.Name = "CheckBoxIDOnly"
+        CheckBoxIDOnly.Size = New Size(188, 36)
+        CheckBoxIDOnly.TabIndex = 9
+        CheckBoxIDOnly.Text = "Copy ID Only"
+        CheckBoxIDOnly.UseVisualStyleBackColor = True
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("High Tower Text", 28.125F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(95, 22)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(596, 89)
+        Label3.TabIndex = 10
+        Label3.Text = "Report Formatter"
+        ' 
+        ' MainApp
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(787, 840)
+        Controls.Add(Label3)
+        Controls.Add(CheckBoxIDOnly)
+        Controls.Add(CheckBoxDate)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(GroupBox1)
+        Controls.Add(txtExploit)
+        Controls.Add(txtUsername)
+        Controls.Add(btnClear)
+        Controls.Add(btnCopy)
+        Controls.Add(btnFetchID)
+        Name = "MainApp"
+        Text = "MainApp"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnFetchID As Button
+    Friend WithEvents btnCopy As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtExploit As TextBox
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lbl_Clip As Label
+    Friend WithEvents lbl_Date As Label
+    Friend WithEvents lbl_Exploit As Label
+    Friend WithEvents lbl_ID As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents CheckBoxDate As CheckBox
+    Friend WithEvents CheckBoxIDOnly As CheckBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ ReportCompilerApp/MainApp.vb
@@ -0,0 +1,159 @@
+﻿Imports System.Net
+Imports System.Net.Http
+Imports System.Text
+Imports Newtonsoft.Json
+Imports System.Text.RegularExpressions
+
+Public Class MainApp
+    Dim prevUsername As String = ""
+    Dim prevID As String = ""
+    Dim formatted As Boolean = False
+
+    Private Shared Function DisplayError(message As String)
+        MessageBox.Show(message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Return Nothing
+    End Function
+
+    Private Sub TxtExploit_TextChanged(sender As Object, e As EventArgs) Handles txtExploit.TextChanged
+        lbl_Exploit.Text = "Exploit: " & txtExploit.Text
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        CheckBoxDate.Checked = False
+        CheckBoxIDOnly.Checked = False
+        lbl_Date.Text = "Date:"
+        lbl_Exploit.Text = "Exploit:"
+        lbl_ID.Text = "ID:"
+        txtExploit.Text = ""
+        txtUsername.Text = ""
+        formatted = False
+    End Sub
+
+    Private Sub MainApp_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lbl_Clip.Location = New Point(lbl_Clip.Location.X, lbl_Clip.Location.Y - 42)
+        lbl_Date.Visible = False
+    End Sub
+
+    Private Sub BtnCopy_Click(sender As Object, e As EventArgs) Handles btnCopy.Click
+        If formatted = False Then
+            If txtUsername.Text = "" Then
+                DisplayError("Please enter a username!")
+                Exit Sub
+            End If
+            Dim userID As String = FetchID(txtUsername.Text)
+            If userID <> "" Then
+                lbl_ID.Text = "ID: " & userID
+                prevUsername = txtUsername.Text
+                prevID = userID
+                formatted = True
+            Else
+                Exit Sub
+            End If
+        End If
+
+        If formatted = True Then
+            If CheckBoxIDOnly.Checked = True Then
+                Dim userID As String = Regex.Replace(lbl_ID.Text, "[^\d]", "")
+                If userID <> "" Then
+                    Clipboard.SetText(userID)
+                    MessageBox.Show("Copied to clipboard!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+                Else
+                    DisplayError("Could not retrieve user ID.")
+                End If
+                Exit Sub
+            End If
+
+            If txtExploit.Text = "" Then
+                DisplayError("Please enter an exploit!")
+                Exit Sub
+            End If
+
+            Dim TextToCopy As String = lbl_ID.Text & vbCrLf & lbl_Exploit.Text
+            If CheckBoxDate.Checked = True Then
+                TextToCopy = TextToCopy & vbCrLf & lbl_Date.Text
+            End If
+            TextToCopy = TextToCopy & vbCrLf & lbl_Clip.Text
+            Clipboard.SetText(TextToCopy)
+            MessageBox.Show("Copied to clipboard!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Exit Sub
+        End If
+    End Sub
+
+    Private Sub CheckBoxDate_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBoxDate.CheckedChanged
+        If CheckBoxDate.Checked = True Then
+            lbl_Date.Visible = True
+            lbl_Clip.Location = New Point(lbl_Clip.Location.X, lbl_Clip.Location.Y + 42)
+            lbl_Date.Text = "Date: " & DateTime.Now.ToString("dd/MM/yyyy")
+        Else
+            lbl_Date.Visible = False
+            lbl_Clip.Location = New Point(lbl_Clip.Location.X, lbl_Clip.Location.Y - 42)
+        End If
+    End Sub
+
+    Private Sub BtnFetchID_Click(sender As Object, e As EventArgs) Handles btnFetchID.Click
+        If txtUsername.Text = "" Then
+            DisplayError("Please enter a username!")
+            Exit Sub
+        ElseIf txtUsername.Text = prevUsername Then
+            formatted = True
+            lbl_ID.Text = "ID: " & prevID
+        Else
+            Dim userID As String = FetchID(txtUsername.Text)
+            If userID <> "" Then
+                lbl_ID.Text = "ID: " & userID
+                formatted = True
+                prevUsername = txtUsername.Text
+                prevID = userID
+            Else
+                Exit Sub
+            End If
+        End If
+    End Sub
+
+    Private Shared Function FetchID(username As String) As String
+        Dim url As String = "https://users.roblox.com/v1/usernames/users"
+        Dim excludeBannedUsers As Boolean = True
+        Dim usernames As String() = {username}
+        Dim data As New With {usernames, excludeBannedUsers}
+        Dim jsonContent As New StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")
+        Try
+            Using httpClient As New HttpClient()
+                Dim response As HttpResponseMessage = httpClient.PostAsync(url, jsonContent).Result
+                If response.IsSuccessStatusCode Then
+                    Dim jsonResponse As String = response.Content.ReadAsStringAsync().Result
+                    Dim result = JsonConvert.DeserializeObject(jsonResponse)
+                    If result("data").Count > 0 Then
+                        Dim userId = result("data")(0)("id").ToString()
+                        If Not String.IsNullOrEmpty(userId) Then
+                            Return userId
+                        Else
+                            DisplayError("Could not retrieve user ID.")
+                            Return ""
+                        End If
+                    Else
+                        DisplayError("No user found.")
+                        Return ""
+                    End If
+                ElseIf response.StatusCode = HttpStatusCode.BadRequest Then
+                    DisplayError("Too many usernames.")
+                    Return ""
+                Else
+                    DisplayError(response.StatusCode.ToString())
+                    Return ""
+                End If
+            End Using
+        Catch ex As Exception
+            DisplayError(ex.Message)
+            Return ""
+        End Try
+    End Function
+
+    Private Sub TxtUsername_TextChanged(sender As Object, e As EventArgs) Handles txtUsername.TextChanged
+        If txtUsername.Text <> prevUsername Then
+            formatted = False
+        Else
+            formatted = True
+            lbl_ID.Text = "ID: " & prevID
+        End If
+    End Sub
+End Class
